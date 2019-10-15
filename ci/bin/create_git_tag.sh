87e601e92b078f37646f6fc8554cb5c5cdfc8dd5
Qj7 <vladimir.dolgiy@apliteni.com>
2019-10-15 03:31:40 +0300

    add v letter to tag

--- ci/bin/create_git_tag.sh
+++ ci/bin/create_git_tag.sh
@@ -5,4 +5,4 @@ RELEASE_VERSION="$(cat $ROOT_DIR/RELEASE_VERSION)"
 
 set -e -x -o pipefail
 
-curl -X POST --show-error --fail "https://gitlab.x.apli.tech/api/v4/projects/${CI_PROJECT_ID}/repository/tags?tag_name=${RELEASE_VERSION}&ref=${CI_COMMIT_SHA}&private_token=XAsxU6xSgacDfNxH5_Wj"
+curl -X POST --show-error --fail "https://gitlab.x.apli.tech/api/v4/projects/${CI_PROJECT_ID}/repository/tags?tag_name=v${RELEASE_VERSION}&ref=${CI_COMMIT_SHA}&private_token=XAsxU6xSgacDfNxH5_Wj"
