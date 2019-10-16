0393df3f7da1b4e97dbfa324209bdafe9acc512e
Qj7 <vladimir.dolgiy@apliteni.com>
2019-10-16 16:19:39 +0300

    add docker specific fix

--- ci/bin/create_git_tag.sh
+++ ci/bin/create_git_tag.sh
@@ -6,3 +6,9 @@ RELEASE_VERSION="$(cat $ROOT_DIR/RELEASE_VERSION)"
 set -e -x -o pipefail
 
 curl -X POST --show-error --fail "https://gitlab.x.apli.tech/api/v4/projects/${CI_PROJECT_ID}/repository/tags?tag_name=v${RELEASE_VERSION}&ref=${CI_COMMIT_SHA}&private_token=${GITLAB_TOKEN}"
+
+if curl --fail -X POST -d@myfile.txt server-URL; then
+    # …(success)
+else
+    # …(failure)
+fi;
