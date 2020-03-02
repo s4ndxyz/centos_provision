ca6c629278d2e3a1f4db891a7d9ad970cdc36d92
Vladimir Dolgiy <vladimir.dolgiy@apliteni.com>
2020-03-02 16:10:26 +0300

    INST-100 fix firewall for digitalocean

--- scripts/src/app/installer/stage4.sh
+++ scripts/src/app/installer/stage4.sh
@@ -10,6 +10,9 @@ stage4(){
   if isset "$AUTO_INSTALL"; then
     debug "Skip reading vars from stdin"
   else
+    if isset "${VARS['rhel_version']}" && [ "${VARS['rhel_version']}" == "8" ]; then
+      install_package iptables
+    fi  
     get_user_vars
   fi
   write_inventory_file
